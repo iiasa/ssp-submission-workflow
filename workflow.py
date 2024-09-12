26ef36a084cc067cc4e9b92342731521a7496578
Philip Hackstock <20710924+phackstock@users.noreply.github.com>
2024-09-12 12:02:54 +0200

    Add MAGICC

--- workflow.py
+++ workflow.py
@@ -1,6 +1,25 @@
+import logging
+import sys
 from pathlib import Path
 import pyam
 from nomenclature import DataStructureDefinition, RegionProcessor, process
+
+
+try:
+    from climate_processor import MAGICCProcessor
+
+    FOUND_MAGICC = True
+except ImportError:
+    FOUND_MAGICC = False
+
+
+logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
+logFormatter = logging.Formatter("%(asctime)s [%(levelname)-5.5s] %(message)s")
+log = logging.getLogger()
+log.handlers.clear()
+consoleHandler = logging.StreamHandler(sys.stdout)
+consoleHandler.setFormatter(logFormatter)
+log.addHandler(consoleHandler)
 
 
 here = Path(__file__).absolute().parent
@@ -11,5 +30,21 @@ def main(df: pyam.IamDataFrame) -> pyam.IamDataFrame:
 
     # Run the validation and region-processing
     dsd = DataStructureDefinition(here / "definitions")
-    processor = RegionProcessor.from_directory(path=here / "mappings", dsd=dsd)
-    return process(df, dsd, processor=processor)
+    processors = [RegionProcessor.from_directory(path=here / "mappings", dsd=dsd)]
+    if FOUND_MAGICC:
+        processors.append(
+            MAGICCProcessor(
+                run_type="complete",
+                magicc_worker_number=8,
+            )
+        )
+        try:
+            return process(df, dsd, processor=processors)
+        except Exception as e:
+            log.warning(
+                (
+                    "Error with MAGICC processing, repeat processing without MAGICC, "
+                    f"details: {e}"
+                )
+            )
+    return process(df, dsd, processor=processors)
